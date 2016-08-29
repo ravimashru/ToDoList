da57e824b086c74cce785440cfffc23c8fc6f67d
mashruravi <mashru.ravi@gmail.com>
2016-08-30 00:07:58 +0530

    Show user lists in default route view

--- app/controller/ToDoDetail.controller.js
+++ app/controller/ToDoDetail.controller.js
@@ -5,7 +5,21 @@ sap.ui.define(["tdl/controller/BaseController"],
 
   return BaseController.extend("tdl.controller.ToDoDetail", {
 
-    
+    onInit: function() {
+      
+      var oModel = new JSONModel();
+
+      var req = jQuery.ajax({
+        url: "/lists",
+        type: "GET"
+      });
+
+      req.done(function(oData){
+        oModel.setData(oData);
+      });
+
+      this.getView().setModel(oModel);
+    }
 
   });
 
